f6c54436d78aa9742eb1b3ccdb43588557289e07
stats <stasafanasev21220@gmail.com>
2022-06-03 09:18:59 +0300

    docker

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,35 +1,69 @@
-import {Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, UsePipes, ValidationPipe} from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode, NotFoundException,
+	Param,
+	Patch,
+	Post, UseGuards,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common';
 import {ProductModel} from './product.model';
 import {FindProductDto} from './dto/find-product.dto';
 import {CreateProductDto} from './dto/create-product.dto';
+import {ProductService} from './product.service';
+import {PRODUCT_NOT_FOUND_ERROR} from './product.constants';
+import {IdValidationPipe} from '../pipes/id-validation.pipe';
+import {JwtAuthGuard} from '../auth/guards/jwt.guard';
 
 @Controller('product')
 export class ProductController {
+	constructor(private readonly productService: ProductService) {
+	}
 
+	@UseGuards(JwtAuthGuard)
 	@UsePipes(new ValidationPipe())
 	@Post('create')
 	async create(@Body() dto: CreateProductDto) {
-
+		return this.productService.create(dto);
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get(':id')
-	async get(@Param('id') id: string) {
-
+	async get(@Param('id', IdValidationPipe) id: string) {
+		const product = this.productService.findById(id);
+		if (!product) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
+		return product;
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
-	async delete(@Param('id') id: string) {
-
+	async delete(@Param('id', IdValidationPipe) id: string) {
+		const deletedProduct = this.productService.deleteById(id);
+		if (!deletedProduct) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
 	}
 
+	@UseGuards(JwtAuthGuard)
+	@UsePipes(new ValidationPipe())
 	@Patch(':id')
-	async patch(@Param('id') id: string, @Body() dto: ProductModel) {
-
+	async patch(@Param('id', IdValidationPipe) id: string, @Body() dto: CreateProductDto) {
+		const updatedProduct = this.productService.updateById(id, dto);
+		if (!updatedProduct) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
+		return updatedProduct;
 	}
 
+	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('find')
 	async find(@Body() dto: FindProductDto) {
-
+		return this.productService.findWithReviews(dto);
 	}
 }
---
+++ src/top-page/top-page.constants.ts
@@ -0,0 +1,1 @@
+export const NOT_FOUND_TOP_PAGE_ERROR = 'страница не найдена'
